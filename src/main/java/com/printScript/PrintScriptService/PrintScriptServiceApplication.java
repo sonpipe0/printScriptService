a3ec19fef8e32c04b856d0550c1268e149e87b07
sonpipe0 <pipefc2003@gmail.com>
2024-09-24 23:03:59 -0300

    setup

---
+++ src/main/java/com/printScript/PrintScriptService/PrintScriptServiceApplication.java
@@ -0,0 +1,13 @@
+package com.printScript.PrintScriptService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintScriptServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintScriptServiceApplication.class, args);
+	}
+
+}
