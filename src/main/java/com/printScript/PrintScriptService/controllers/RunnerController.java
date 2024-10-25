0e0ec0f48b9da68550ee9b031c8c23df4bdbdfb3
sonpipe0 <pipefc2003@gmail.com>
2024-10-24 23:05:46 -0300

    added excute and test methods

--- src/main/java/com/printScript/PrintScriptService/controllers/RunnerController.java
+++ src/main/java/com/printScript/PrintScriptService/controllers/RunnerController.java
@@ -3,6 +3,7 @@ package com.printScript.PrintScriptService.controllers;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Map;
 
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.printScript.PrintScriptService.DTO.ExecuteContextDTO;
 import com.printScript.PrintScriptService.DTO.Response;
+import com.printScript.PrintScriptService.DTO.TestContextDTO;
 import com.printScript.PrintScriptService.DTO.ValidateRequestDTO;
 import com.printScript.PrintScriptService.error.ParsingError;
 import com.printScript.PrintScriptService.services.RunnerService;
@@ -35,6 +38,42 @@ public class RunnerController {
         String code = validateRequestDTO.getCode();
         String version = validateRequestDTO.getVersion();
         return getObjectResponseEntity(version, code);
+    }
+
+    @PostMapping("/execute")
+    public ResponseEntity<Object> execute(@RequestBody ExecuteContextDTO executeContextDTO) {
+        Response<List<String>> result = getExectutionResponse(executeContextDTO);
+
+        if (result.isError()) {
+            return ResponseEntity.status(result.getError().code()).body(result.getError().message());
+        } else {
+            return new ResponseEntity<>(result.getData(), HttpStatus.OK);
+        }
+    }
+
+    @PostMapping("/test")
+    public ResponseEntity<Object> executeTest(@RequestBody TestContextDTO testContextDTO) {
+        ExecuteContextDTO executeContextDTO = new ExecuteContextDTO(testContextDTO.getText(),
+                testContextDTO.getVersion(), testContextDTO.getInputs(), testContextDTO.getEnvVars());
+        Response<List<String>> result = getExectutionResponse(executeContextDTO);
+
+        if (result.isError()) {
+            return ResponseEntity.status(result.getError().code()).body(result.getError().message());
+        }
+        if (result.getData().equals(testContextDTO.getExpected())) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+        }
+    }
+
+    private Response<List<String>> getExectutionResponse(ExecuteContextDTO executeContextDTO) {
+        String code = executeContextDTO.getText();
+        String version = executeContextDTO.getVersion();
+        List<String> inputs = executeContextDTO.getInputs();
+        Map<String, String> envVars = executeContextDTO.getEnvVars();
+        Response<List<String>> result = runnerService.execute(code, version, inputs, envVars);
+        return result;
     }
 
     @NotNull
--- src/main/java/com/printScript/PrintScriptService/services/RunnerService.java
+++ src/main/java/com/printScript/PrintScriptService/services/RunnerService.java
@@ -5,15 +5,21 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
 import com.printScript.PrintScriptService.DTO.Response;
+import com.printScript.PrintScriptService.error.Error;
 import com.printScript.PrintScriptService.error.ParsingError;
 
 import dataObjects.ParsingResult;
+import factories.Runner;
 import factories.ValidatorFactory;
+import utils.InterpreterResult;
+import utils.MainStringInputProvider;
 import utils.PercentageCollector;
+import utils.StringInputProvider;
 
 @Service
 public class RunnerService {
@@ -35,5 +41,24 @@ public class RunnerService {
         } else {
             return Response.withData(errorList);
         }
+    }
+
+    public Response<List<String>> execute(String text, String version, List<String> inputs,
+            Map<String, String> envVars) {
+        InputStream code = new ByteArrayInputStream(text.getBytes());
+        StringInputProvider provider = new MainStringInputProvider(inputs.iterator());
+        Runner runner = new Runner();
+        Iterator<InterpreterResult> results = runner.run(code, version, provider, envVars, false).iterator();
+        List<String> output = new ArrayList<>();
+        while (results.hasNext()) {
+            InterpreterResult result = results.next();
+            if (result.hasPrintln()) {
+                output.add(result.getPrintln());
+            }
+            if (result.hasException()) {
+                return Response.withError(new Error(500, "The execution failed, please check the inputs"));
+            }
+        }
+        return Response.withData(output);
     }
 }
