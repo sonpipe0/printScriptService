1d852d399d79c94dc1abc8cba3876e5acb58ff50
Pedrodeforonda <pdeforonda@mail.austral.edu.ar>
2024-11-21 00:03:22 -0300

    tests

--- src/main/java/com/printScript/PrintScriptService/DTO/ValidateRequestDTO.java
+++ src/main/java/com/printScript/PrintScriptService/DTO/ValidateRequestDTO.java
@@ -1,10 +1,12 @@
 package com.printScript.PrintScriptService.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class ValidateRequestDTO {
 
     private String code;
--- src/main/java/com/printScript/PrintScriptService/auth/OAuth2ResourceServerSecurityConfiguration.java
+++ src/main/java/com/printScript/PrintScriptService/auth/OAuth2ResourceServerSecurityConfiguration.java
@@ -5,6 +5,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,6 +20,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@Profile("!test")
 public class OAuth2ResourceServerSecurityConfiguration {
 
     private final String audience;
